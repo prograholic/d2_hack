f8e0a930eaef8f56cad62587298f2a060fe24150
Alexey Kutumov <alexey.kutumov@gmail.com>
2019-12-06 16:02:13 +0700

    Added missing dependencies

--- d2_hack/TutorialApplication.cpp
+++ d2_hack/TutorialApplication.cpp
@@ -177,20 +177,24 @@ void TutorialApplication::defineTerrain(long x, long y)
 
 int main(int argc, char *argv[])
 {
-  try
-  {
-    TutorialApplication app;
+    try
+    {
+        TutorialApplication app;
+        
+        app.initApp();
+        app.getRoot()->startRendering();
+        app.closeApp();
+    }
+    catch(const Ogre::Exception& e)
+    {
+        std::cerr << "An OGRE exception has occured: " << e.getFullDescription().c_str() << std::endl;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "An exception has occured: " << e.what() << std::endl;
+    }
     
-    app.initApp();
-    app.getRoot()->startRendering();
-    app.closeApp();
-  }
-  catch(const Ogre::Exception & e)
-  {
-    std::cerr << "An exception has occured: " <<
-                 e.getFullDescription().c_str() << std::endl;
-  }
-  return 0;
+    return 0;
 }
 
 
