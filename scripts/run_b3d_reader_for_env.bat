117dff8e4794f9b6be617fdcdb992ffe04cda242
Alexey Kutumov <alexey.kutumov@gmail.com>
2024-03-02 21:48:16 +0700

    Update scripts

--- scripts/run_b3d_reader_for_env.bat
+++ scripts/run_b3d_reader_for_env.bat
@@ -1,2 +1,4 @@
-d2_hack.resource.data.tool.b3d_reader.exe ENV %1    --skip_face_info --skip_mesh_info --skip_vector_data > %1.log 2>NUL
+d2_hack.resource.data.tool.b3d_reader.exe ENV %1 --skip_face_info --skip_mesh_info --skip_vector_data > %1.log 2>NUL
+d2_hack.resource.data.tool.b3d_reader.exe ENV %1 --skip_optimization --skip_transformation --skip_face_info --skip_mesh_info --skip_vector_data --print_only_names > %1_names_noopt.log 2>NUL
+d2_hack.resource.data.tool.b3d_reader.exe ENV %1 --skip_face_info --skip_mesh_info --skip_vector_data --print_only_names > %1_names.log 2>NUL
 d2_hack.resource.data.tool.b3d_reader.exe ENV %1 --skip_optimization --skip_transformation  --skip_face_info --skip_mesh_info --skip_vector_data > %1_noopt.log
---
+++ scripts/run_b3d_reader_for_types.bat
@@ -0,0 +1,2 @@
+d2_hack.resource.data.tool.b3d_reader.exe skip skip --skip_face_info --skip_mesh_info --skip_vector_data --print_only_types > types.log
+d2_hack.resource.data.tool.b3d_reader.exe skip skip --skip_optimization --skip_transformation  --skip_face_info --skip_mesh_info --skip_vector_data --print_only_types > types_noopt.log
