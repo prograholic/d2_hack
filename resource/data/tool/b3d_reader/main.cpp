9eb027841b6ec12208c9bb64799b15ab9f46c6b6
Alexey Kutumov <alexey.kutumov@gmail.com>
2025-05-23 22:41:44 +0700

    Fix exception handling for incorrect options Also fix naming for params

--- resource/data/tool/b3d_reader/main.cpp
+++ resource/data/tool/b3d_reader/main.cpp
@@ -16,33 +16,33 @@
 
 int main(int argc, char* argv[])
 {
-    namespace po = boost::program_options;
-
-    po::options_description general("Allowed options");
-
-    general.add_options()
-        ("help", "Produce help message")
-        (options::mode, po::value<std::string>(), "select mode")
-        (options::subdir, po::value<std::string>(), "select subdir for b3d file")
-        (options::id, po::value<std::string>(), "select id")
-        (options::transform, "Perform transformation")
-        (options::optimize, "Perform optimization")
-        (options::only_common, "Process only common.b3d")
-        (options::only_trucks, "Process ony trucks.b3d")
-        (options::use_single_player_registry, "Use single player registry");
-
-    general.add(get_print_options());
-    general.add(get_analyze_options());
-
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, general), vm);
-    po::notify(vm);
-
-    Ogre::LogManager logMgr;
-    logMgr.createLog("default", true, false, true);
-
     try
     {
+        namespace po = boost::program_options;
+
+        po::options_description general("Allowed options");
+
+        general.add_options()
+            ("help", "Produce help message")
+            (options::mode, po::value<std::string>(), "select mode")
+            (options::subdir, po::value<std::string>(), "select subdir for b3d file")
+            (options::id, po::value<std::string>(), "select id")
+            (options::skip_transformation, "Skip transformation")
+            (options::skip_optimization, "Skip optimization")
+            (options::only_common, "Process only common.b3d")
+            (options::only_trucks, "Process ony trucks.b3d")
+            (options::use_single_player_registry, "Use single player registry");
+
+        general.add(get_print_options());
+        general.add(get_analyze_options());
+
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, general), vm);
+        po::notify(vm);
+
+        Ogre::LogManager logMgr;
+        logMgr.createLog("default", true, false, true);
+
         using namespace d2_hack::resource::data::b3d;
 
         B3dRegistry registry
@@ -72,11 +72,11 @@ int main(int argc, char* argv[])
 
         B3dForest forest = ReadB3d(registry);
 
-        if (vm.count(options::transform))
+        if (!vm.count(options::skip_transformation))
         {
             transformation::Transform(forest);
         }
-        if (vm.count(options::optimize))
+        if (!vm.count(options::skip_optimization))
         {
             transformation::Optimize(forest);
         }
--- scripts/run_b3d_reader_for_common.bat
+++ scripts/run_b3d_reader_for_common.bat
@@ -1,4 +1,4 @@
-d2_hack.resource.data.tool.b3d_reader.exe --mode print --use_single_player_registry --only_common --skip_face_info --skip_mesh_info --skip_vector_data > common.log
-d2_hack.resource.data.tool.b3d_reader.exe --mode print --use_single_player_registry --only_common > common_full.log
-d2_hack.resource.data.tool.b3d_reader.exe --mode print --use_single_player_registry --only_common --skip_optimization --skip_transformation  --skip_face_info --skip_mesh_info --skip_vector_data > common_noopt.log
-d2_hack.resource.data.tool.b3d_reader.exe --mode print --use_single_player_registry --only_common --skip_optimization --skip_transformation > common_noopt_full.log
+d2_hack.resource.data.tool.b3d_reader.exe --mode print  --only_common --skip_face_info --skip_mesh_info --skip_vector_data > common.log
+d2_hack.resource.data.tool.b3d_reader.exe --mode print  --only_common > common_full.log
+d2_hack.resource.data.tool.b3d_reader.exe --mode print  --only_common --skip_optimization --skip_transformation  --skip_face_info --skip_mesh_info --skip_vector_data > common_noopt.log
+d2_hack.resource.data.tool.b3d_reader.exe --mode print  --only_common --skip_optimization --skip_transformation > common_noopt_full.log
